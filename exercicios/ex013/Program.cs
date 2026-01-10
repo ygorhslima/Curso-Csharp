739d06ef088bbec014ca34e55b6d57ad47c630f1
Ygor Henrique <ygorlima.dev06@gmail.com>
2026-01-10 12:20:22 -0300

    fazendo quatro novos exercícios

---
+++ exercicios/ex013/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main()
+    {
+        float peso, altura, IMC;
+        
+        Console.Write("Digite o peso: ");
+        peso = float.Parse(Console.ReadLine());
+        
+        Console.Write("Digite a altura: ");
+        altura = float.Parse(Console.ReadLine());
+
+        IMC = peso / (altura * altura);
+
+        if (IMC < 20)
+        {
+            Console.Write("Abaixo do peso");
+        }
+        else if (IMC <= 25)
+        {
+            Console.Write("Normal");
+        }
+        else if (IMC <= 30)
+        {
+            Console.Write("excesso de peso");
+        }
+        else if (IMC <= 35)
+        {
+            Console.Write("obesidade");
+        }
+        else
+        {
+            Console.Write("obsedidade Mórbida");
+        }
+
+    }
+}
---
+++ exercicios/ex014/Program.cs
@@ -0,0 +1,59 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int cargo;
+        double salario, reajuste, aumento;
+
+        Console.WriteLine("---------------------------");
+        Console.WriteLine("[1] Serviços gerais");
+        Console.WriteLine("[2] Vigia");
+        Console.WriteLine("[3] Recepcionista");
+        Console.WriteLine("[4] Vendedor");        
+        Console.WriteLine("---------------------------");
+
+        Console.WriteLine("Digite o cargo: ");
+        cargo = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o salário: ");
+        salario = double.Parse(Console.ReadLine());
+
+
+        if(cargo == 1)
+        {
+            Console.WriteLine("Serviços Gerais");
+            aumento = salario * (50.0/100.0);
+            reajuste = salario + aumento;
+
+            Console.WriteLine($"O aumento é de: {aumento}");
+            Console.WriteLine($"O reajuste é de: {reajuste}");
+        }
+        if(cargo == 2)
+        {
+            Console.WriteLine("Vigia");
+            aumento = salario * (30.0/100.0);
+            reajuste = salario + aumento;
+
+            Console.WriteLine($"O aumento é de: {aumento}");
+            Console.WriteLine($"O reajuste é de: {reajuste}");
+        }
+        if(cargo == 3)
+        {
+            Console.WriteLine("Recepcionista");
+            aumento = salario * (25.0/100.0);
+            reajuste = salario + aumento;
+
+            Console.WriteLine($"O aumento é de: {aumento}");
+            Console.WriteLine($"O reajuste é de: {reajuste}");
+        }
+        if(cargo == 4)
+        {
+            Console.WriteLine("Vendedor");
+            aumento = salario * (15.0/100.0);
+            reajuste = salario + aumento;
+
+            Console.WriteLine($"O aumento é de: {aumento}");
+            Console.WriteLine($"O reajuste é de: {reajuste}");
+        }
+    }
+}
