567c849e5ea2e9ae9eb1bc60a41ac02b251ab2d7
Ygor Henrique <ygorlima.dev06@gmail.com>
2026-01-16 10:13:41 -0300

    terminando exercício com função

---
+++ exercicios/ex022/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+
+class Program
+{
+
+
+    public static int Soma(int x)
+    {
+        int i;
+        int total = 0;
+        for (i = 1; i < x; i++)
+        {
+            total += i;
+        }
+        return total;
+    }
+
+    static void Main()
+    {
+        Console.Write("Informe um número: ");
+        int n = int.Parse(Console.ReadLine());
+        int resultado = Soma(n);
+        Console.WriteLine($" o somatório é: {resultado}");
+    }
+
+}
