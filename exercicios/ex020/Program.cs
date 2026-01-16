41ee701fb02b7a3e023139a601c049a8cc2174e7
Ygor Henrique <ygorlima.dev06@gmail.com>
2026-01-16 09:57:20 -0300

    criando exemplos com array

---
+++ exercicios/ex020/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics.Contracts;
+
+class Program
+{
+    public static void Main()
+    {
+        int tamanho = 10;
+        string[] nome = new string[tamanho];
+        float[] n1 = new float[tamanho];
+        float[] n2 = new float[tamanho];
+        float[] n3 = new float[tamanho];
+        float[] n4 = new float[tamanho];
+        float[] media = new float[tamanho];
+        int contador;
+
+        for (contador = 0; contador < tamanho; contador++)
+        {
+            Console.Write("Informe o nome do aluno: ");
+            nome[contador] = Console.ReadLine();
+
+            Console.Write("nota 1: ");
+            n1[contador] = float.Parse(Console.ReadLine());
+
+            Console.Write("nota 2: ");
+            n2[contador] = float.Parse(Console.ReadLine());
+            
+            Console.Write("nota 3: ");
+            n3[contador] = float.Parse(Console.ReadLine());
+            
+            Console.Write("nota 4: ");
+            n4[contador] = float.Parse(Console.ReadLine());
+            
+            media[contador] = (n1[contador] + n2[contador] + n3[contador] + n4[contador]) / 4;
+        }
+        for(contador = 0; contador < tamanho; contador++)
+        {
+            Console.WriteLine($"Aluno.: {nome[contador]}");
+            Console.WriteLine($"nota 1: {n1[contador]}");
+            Console.WriteLine($"nota 2: {n2[contador]}");
+            Console.WriteLine($"nota 3: {n3[contador]}");
+            Console.WriteLine($"nota 4: {n4[contador]}");
+            Console.WriteLine($"Média.: {media[contador]}");
+            
+        }
+    }
+}
---
+++ exercicios/ex021/Program.cs
@@ -0,0 +1,44 @@
+﻿//método sequencial no C#
+
+class Program
+{
+    static void Main()
+    {
+        int[] vnum = new int[20];
+        int num;
+        bool acha;
+        int i;
+
+        for (i = 1; i < 20; i++)
+        {
+            Console.Write("Informe um número: ");
+            vnum[i] = int.Parse(Console.ReadLine());
+        }
+        
+        Console.Write("Informe o número que deseja encontrar: ");
+        num = int.Parse(Console.ReadLine());
+
+        i = 1;
+        acha = false;
+
+        while (i <= 20 && acha == false)
+        {
+            if (num == vnum[i])
+            {
+                acha = true;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        if (acha == true)
+        {
+            Console.WriteLine($"O elemento foi encontrado na posição {i}");
+        }
+        else
+        {
+            Console.WriteLine("o elemento não foi encontrado");
+        }
+    }
+}
