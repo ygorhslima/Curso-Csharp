c7587dedb9082fd08e6a264c1abb975513c306a7
Ygor Henrique <ygorlima.dev06@gmail.com>
2026-01-18 17:12:01 -0300

    praticando objetos na prática

---
+++ exercicios/ex024/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ex024
+{
+
+
+    public class CadAluno
+    {
+        public string Nome {get;set;}
+        public float[] Notas {get;set;}
+    }
+
+
+    public class Program{
+        public static void Main()
+        {
+            int tam = 2;
+            CadAluno[] alunos = new CadAluno[tam];
+            
+            int i,j;
+
+            for(i = 0; i < alunos.Length; i++)
+            {
+                alunos[i] = new(){Notas = new float[4]};
+
+                Console.Write("informe o nome do aluno: ");
+                alunos[i].Nome = Console.ReadLine();
+
+                for(j = 0; j < alunos[i].Notas.Length; j++)
+                {
+                    Console.Write($"informe a nota {j+1}: ");
+                    alunos[i].Notas[j] = float.Parse(Console.ReadLine());
+                }
+            }
+
+            for(i = 0; i < alunos.Length; i++)
+            {
+                
+                Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");   
+                Console.WriteLine($"Aluno: {alunos[i].Nome}");
+
+                for(j = 0; j < alunos[i].Notas.Length; j++)
+                {
+                    Console.WriteLine($"Nota {j+1}: {alunos[i].Notas[j]}");
+                }
+                
+                Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+            }
+        }
+    }
+}
