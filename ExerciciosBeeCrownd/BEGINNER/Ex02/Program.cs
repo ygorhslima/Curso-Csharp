b4b4e3e078e372e2bdde26065b7deb26607dcd96
Ygor Henrique <ygorlima.dev06@gmail.com>
2026-02-12 14:58:55 -0300

    começando os primeiros desafios

---
+++ ExerciciosBeeCrownd/BEGINNER/Ex02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        /**
+         * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí
+         */
+        double R = double.Parse(Console.ReadLine());
+        double A = 3.14159 * R*R;
+        Console.WriteLine($"A={A:F4}");
+    }
+}
