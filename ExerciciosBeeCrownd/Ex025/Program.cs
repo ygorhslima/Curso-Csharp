5acf8a6584b2f2e52c8976cea778f41e2fb8c357
Ygor Henrique <ygorlima.dev06@gmail.com>
2026-03-03 20:11:54 -0300

    Update Program.cs

--- ExerciciosBeeCrownd/Ex025/Program.cs
+++ ExerciciosBeeCrownd/Ex025/Program.cs
@@ -11,7 +11,7 @@ class URI
          * Code your solution here
          * Escriba su solución aquí
          */
-        //Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).
+        //Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).f
 
         string[] linha = Console.ReadLine().Split(' ');
         double x = double.Parse(linha[0]);
